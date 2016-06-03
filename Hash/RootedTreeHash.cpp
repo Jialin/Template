829b19e473221c81c4c49eefebec79297bb26df1
Jialin <Jialin.Ouyang@gmail.com>
2016-06-02 23:23:31 -0700

    Add duplicate edges config in RootedTreeHash

--- Hash/RootedTreeHash.cpp
+++ Hash/RootedTreeHash.cpp
@@ -6,19 +6,18 @@
  *   where H1, H2, ... are the sorted hash values of its child nodes.
  *
  * Usage:
- *   init(numberOfNodes);
+ *   init(numberOfNodes, whetherDuplicateEdges *OPTIONAL*);
  *   // add edges
- *   preprocess();
  *   calc(rootIdx, seed1, seed2);
  */
 template<
     int MOD = 1000000007,
     unsigned long long SEED1 = 100000000000000003ULL,
     unsigned long long SEED2 = 1000000000000000003ULL> struct RootedTreeHash {
-  vector<pair<int, int> > _allEdges;
   vector<vector<int> > _edges;
   vector<unsigned long long> _hashs;
   unsigned long long _seed1, _seed2;
+  bool _duplicateEdges;
 
   unsigned long long &_dfs(int u, int parent) {
     vector<unsigned long long> childs(_edges[u].size() - (parent >= 0));
@@ -34,14 +33,15 @@ template<
     return res;
   }
 
-  void init(int n) {
-    _allEdges.clear();
-    _edges = vector<vector<int> >(n);
-    _hashs = vector<unsigned long long>(n);
+  void init(int n, bool duplicateEdges = false) {
+    _duplicateEdges = duplicateEdges;
+    _edges.resize(n);
+    for (auto &edges : _edges) edges.clear();
+    _hashs.resize(n);
   }
 
   void addDirectedEdge(int x, int y) {
-    _allEdges.push_back(make_pair(x, y));
+    _edges[x].push_back(y);
   }
 
   void addUndirectedEdge(int x, int y) {
@@ -49,17 +49,18 @@ template<
     addDirectedEdge(y, x);
   }
 
-  void preprocess() {
-    sort(_allEdges.begin(), _allEdges.end());
-    _allEdges.erase(unique(_allEdges.begin(), _allEdges.end()), _allEdges.end());
-    for (auto &edge : _allEdges) _edges[edge.first].push_back(edge.second);
-  }
-
   unsigned long long &calc(
       int root = 0,
       unsigned long long _seed1 = SEED1,
       unsigned long long _seed2 = SEED2) {
 
+    if (_duplicateEdges) {
+      for (auto &edges : _edges) {
+        sort(edges.begin(), edges.end());
+        edges.erase(unique(edges.begin(), edges.end()), edges.end());
+      }
+      _duplicateEdges = false;
+    }
     this->_seed1 = _seed1;
     this->_seed2 = _seed2;
     return _dfs(root, -1);
